af27fefda3abac86f02563a05bc509c4122ce7af
CNA-Bld <cna.bld@gmail.com>
2020-02-06 01:08:15 +0800

    Use os.tmpname to get the temp file path

--- docs/mpv-gif.lua
+++ docs/mpv-gif.lua
@@ -3,29 +3,20 @@
 local msg = require 'mp.msg'
 local utils = require 'mp.utils'
 
-gif_filters = "fps=24"
-mpeg4_gif_filters = ""
-standard_filters = "setsar=1:1"
+local gif_filters = "fps=24"
+local mpeg4_gif_filters = ""
+local standard_filters = "setsar=1:1"
 
-start_time = -1
-end_time = -1
-temp_palette_path = "/tmp/palette.png"
+local start_time = -1
+local end_time = -1
 
 -- shell escape
-function esc(s)
+local function esc(s)
     return string.gsub(s, "'", "'\\''")
 end
 
-function make_mpeg4_gif()
-    make_gif_internal(true)
-end
-
-function make_gif()
-    make_gif_internal(false)
-end
-
-function construct_filter(in_filter, max_aspect)
-    filters = {}
+local function construct_filter(in_filter, max_aspect)
+    local filters = {}
 
     if mp.get_property("deinterlace") == "yes" then
         -- If we are using deinterlace, let ffmpeg do it too.
@@ -33,8 +24,8 @@ function construct_filter(in_filter, max_aspect)
     end
 
     -- These aspects are after filter / manual aspect ratio change, but before output scaling (for window).
-    width = mp.get_property_number("dwidth")
-    height = mp.get_property_number("dheight")
+    local width = mp.get_property_number("dwidth")
+    local height = mp.get_property_number("dheight")
 
     if (width > max_aspect) and (height > max_aspect) then
         -- OK we have to scale it.
@@ -54,7 +45,7 @@ function construct_filter(in_filter, max_aspect)
     return table.concat(filters, ",")
 end
 
-function make_gif_internal(use_mpeg4)
+local function make_gif_internal(use_mpeg4)
     local start_time_l = start_time
     local end_time_l = end_time
     if start_time_l == -1 or end_time_l == -1 or start_time_l >= end_time_l then
@@ -77,7 +68,7 @@ function make_gif_internal(use_mpeg4)
         -- MPEG4_GIF
         output_file_path = output_file_path .. ".mp4"
 
-        filters = construct_filter(mpeg4_gif_filters, 720)
+        local filters = construct_filter(mpeg4_gif_filters, 720)
 
         local args = string.format("ffmpeg -v warning -ss %s -i '%s' -t %s -c:v libx264 -pix_fmt yuv420p -an -filter:v \"%s\" -y '%s'", position, esc(input_file_path), duration, filters, esc(output_file_path))
         msg.info(args)
@@ -86,7 +77,8 @@ function make_gif_internal(use_mpeg4)
         -- Real GIF
         output_file_path = output_file_path .. ".gif"
 
-        filters = construct_filter(gif_filters, 540)
+        local filters = construct_filter(gif_filters, 540)
+        local temp_palette_path = os.tmpname() .. ".png"
 
         -- first, create the palette
         local args = string.format("ffmpeg -v warning -ss %s -t %s -i '%s' -vf '%s,palettegen' -y '%s'", position, duration, esc(input_file_path), filters, esc(temp_palette_path))
@@ -103,14 +95,22 @@ function make_gif_internal(use_mpeg4)
     mp.osd_message(string.format("GIF created.\n%s", output_file_path))
 end
 
-function set_gif_start()
+local function set_gif_start()
     start_time = mp.get_property_number("time-pos", -1)
     mp.osd_message("GIF Start: " .. start_time)
 end
 
-function set_gif_end()
+local function set_gif_end()
     end_time = mp.get_property_number("time-pos", -1)
     mp.osd_message("GIF End: " .. end_time)
+end
+
+local function make_mpeg4_gif()
+    make_gif_internal(true)
+end
+
+local function make_gif()
+    make_gif_internal(false)
 end
 
 mp.add_key_binding("g", "set_gif_start", set_gif_start)
